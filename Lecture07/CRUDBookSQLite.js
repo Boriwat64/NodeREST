a8335dff3cb3383f9094820749565e38ef4f1c9b
Boriwat64 <s6406021620131@email.kmutnb.ac.th>
2023-02-15 11:44:17 +0700

    updeta

---
+++ Lecture07/CRUDBookSQLite.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app =express();
+
+const db =new sqlite3.Database('./Database/Book.sqlite');
+
+app.use(express.json());
+
+db.run(`CREATE TABLE IF NOT EXISTS  Books (
+    id INTEHER PRIMARY KEY,
+    titli  Text,
+    author TEXT
+    )`);
+
+app.get('/books',(req, res)=>{
+    db.all('SELWCT * FROM books', (err, rows) => {
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.json(rows);
+        }
+    });
+});
+
+app.get('/books/:id',(req, res)=>{
+    db.get('SELWCT * FROM books WHERE id = ?', (err, row) => {
+        if(err){
+            res.status(500).send(err);
+        }else{
+            if(!row){
+                res.status(404).send('Books not found');
+            }else{
+                res.json(row);
+            }
+        }
+    });
+});
+
+app.get('/books/:id',(req, res)=>{
+    const book =req.body;
+    db.get('INSERT INTO books (title, author) VALUES(?, ?)', book.title, book.author, function(err) {
+        if(err){
+            res.status(500).send(err);
+        }else{
+           book.id = this.lastID;
+           res.send(book);
+        }
+    });
+});
+
+app.put('/books/:id',(req, res)=>{
+    const book =req.body;
+    db.get('UPDATE books SET title = ? WHERE id = ?', book.title, book.author,req.params.id, function(err) {
+        if(err){
+            res.status(500).send(err);
+        }else{
+           res.send(book);
+        }
+    });
+});
+
+app.delete('/books/:id',(req, res)=>{
+    db.run('DELETE FROM books WHERE id = ?',req.params.id, function(err) {
+        if(err){
+            res.status(500).send(err);
+        }else{
+           res.send({});
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen, () => console.log(`Listening on port ${port}...`);
